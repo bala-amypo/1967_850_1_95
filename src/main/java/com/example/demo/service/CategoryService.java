0750430d8a5ca7a2f00c02de11ba2c69c4c26b6f
bala-amypo <bala@amypo.in>
2025-12-21 07:29:43 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -1,9 +1,25 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
 
-public interface CategoryService {
-    Category addCategory(Category category);
-    List<Category> getAllCategories();
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository repo;
+
+    public CategoryService(CategoryRepository repo) {
+        this.repo = repo;
+    }
+
+    public Category addCategory(Category c) {
+        return repo.save(c);
+    }
+
+    public List<Category> getAllCategories() {
+        return repo.findAll();
+    }
 }
