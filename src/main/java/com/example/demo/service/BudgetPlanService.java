e0feebb61ae1f7d7a73e34702c735887dee1068d
bala-amypo <bala@amypo.in>
2025-12-21 15:35:44 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetPlanService.java
+++ src/main/java/com/example/demo/service/BudgetPlanService.java
@@ -2,3 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.model.*;
 import java.util.List;
+public interface BudgetPlanService {
+    BudgetPlan createBudgetPlan(Long userId, BudgetPlan plan);
+    BudgetPlan getBudgetPlan(Long userId, Integer month, Integer year);
+}
