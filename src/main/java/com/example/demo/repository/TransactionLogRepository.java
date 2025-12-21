0b66b32e0dfa847d26b87c7e71a00d5906ed43f8
bala-amypo <bala@amypo.in>
2025-12-21 16:51:38 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransactionLogRepository.java
+++ src/main/java/com/example/demo/repository/TransactionLogRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.TransactionLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<TransactionLog, Long> {
+public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
 }
