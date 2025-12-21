8556c47135950977447dae60b2acd66fd1a83beb
bala-amypo <bala@amypo.in>
2025-12-21 16:24:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    // Constructor injection ONLY
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public Category addCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+}
