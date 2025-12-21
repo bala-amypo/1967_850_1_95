0fd5c4b990f4d800b8de141e95f1e070ad53b8bb
bala-amypo <bala@amypo.in>
2025-12-21 06:03:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+
+@Entity
+public class TransactionLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Category category;
+
+    private Double amount;
+    private String description;
+    private LocalDate transactionDate;
+
+    public TransactionLog() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+
+    public Category getCategory() { return category; }
+    public void setCategory(Category category) { this.category = category; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getTransactionDate() { return transactionDate; }
+    public void setTransactionDate(LocalDate transactionDate) { this.transactionDate = transactionDate; }
+}
