788246e76cb96140189ed678c5d4e08f88701d11
bala-amypo <bala@amypo.in>
2025-12-21 17:28:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/BudgetSummaryController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.BudgetSummaryService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/budget-summaries")
+public class BudgetSummaryController {
+
+    private final BudgetSummaryService service;
+
+    public BudgetSummaryController(BudgetSummaryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Object getAll() {
+        return service.getAll();
+    }
+}
