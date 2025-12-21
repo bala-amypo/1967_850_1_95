75a82f2d8d1dc143947da11019bd937b71b135b8
bala-amypo <bala@amypo.in>
2025-12-21 17:16:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/CategoryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.service.CategoryService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository repo;
+
+    public CategoryServiceImpl(CategoryRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<?> getAll() {
+        return repo.findAll();
+    }
+}
