ba32980e34ccac2b66d0358869a678b78fb342de
bala-amypo <bala@amypo.in>
2025-12-21 07:25:24 +0000

    User commit

--- src/main/java/com/example/demo/model/BudgetPlan.java
+++ src/main/java/com/example/demo/model/BudgetPlan.java
@@ -14,10 +14,9 @@ public class BudgetPlan {
 
     private Integer month;
     private Integer year;
+
     private Double incomeTarget;
     private Double expenseLimit;
-
-    public BudgetPlan() {}
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
