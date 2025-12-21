23f8d77c14763a744ae919b3811d784bcae9f9df
bala-amypo <bala@amypo.in>
2025-12-21 15:39:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/BudgetPlanRepository.java
+++ src/main/java/com/example/demo/repository/BudgetPlanRepository.java
@@ -6,3 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+public interface BudgetPlanRepository extends JpaRepository<BudgetPlan, Long> {
+    Optional<BudgetPlan> findByUserAndMonthAndYear(User user, Integer month, Integer year);
+}
