58f88107dd63ebe6a6ef5d4017139fb662be1d3f
bala-amypo <bala@amypo.in>
2025-12-21 07:25:10 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -1,7 +1,7 @@
 package com.example.demo.model;
 
-import java.time.LocalDate;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class TransactionLog {
@@ -19,8 +19,6 @@ public class TransactionLog {
     private Double amount;
     private String description;
     private LocalDate transactionDate;
-
-    public TransactionLog() {}
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
