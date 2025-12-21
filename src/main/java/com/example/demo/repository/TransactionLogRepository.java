3d3fb5940e10cd7f59496d8959926b7d9cfae299
bala-amypo <bala@amypo.in>
2025-12-21 06:08:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TransactionLogRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.TransactionLog;
+import com.example.demo.model.User;
+
+public interface TransactionLogRepository
+        extends JpaRepository<TransactionLog, Long> {
+
+    List<TransactionLog> findByUserAndTransactionDateBetween(
+            User user, LocalDate start, LocalDate end);
+}
