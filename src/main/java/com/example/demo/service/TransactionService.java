56ddf1d85f5d2042dc4a8f08f076426ad3151c7f
bala-amypo <bala@amypo.in>
2025-12-21 07:29:56 +0000

    User commit

--- src/main/java/com/example/demo/service/TransactionService.java
+++ src/main/java/com/example/demo/service/TransactionService.java
@@ -1,9 +1,19 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.model.TransactionLog;
+import com.example.demo.repository.TransactionLogRepository;
+import org.springframework.stereotype.Service;
 
-public interface TransactionService {
-    TransactionLog addTransaction(Long userId, TransactionLog log);
-    List<TransactionLog> getUserTransactions(Long userId);
+@Service
+public class TransactionService {
+
+    private final TransactionLogRepository repo;
+
+    public TransactionService(TransactionLogRepository repo) {
+        this.repo = repo;
+    }
+
+    public TransactionLog addTransaction(TransactionLog log) {
+        return repo.save(log);
+    }
 }
