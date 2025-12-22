926c2e91887cbe8832e66928afbdd0e8dc55fcdf
bala-amypo <bala@amypo.in>
2025-12-22 05:32:34 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetSummaryService.java
+++ src/main/java/com/example/demo/service/BudgetSummaryService.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.model.BudgetSummary;
 import java.util.List;
 
 public interface BudgetSummaryService {
+
     List<?> getAll();
+
+    // ✅ ADD
+    BudgetSummary create(BudgetSummary summary);
 }
