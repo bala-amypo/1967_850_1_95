b5210eb37080aa62863fabf8b0ca60bc0c403913
bala-amypo <bala@amypo.in>
2025-12-22 05:32:43 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/BudgetSummaryServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/BudgetSummaryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.BudgetSummary;
 import com.example.demo.repository.BudgetSummaryRepository;
 import com.example.demo.service.BudgetSummaryService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -14,7 +17,15 @@ public class BudgetSummaryServiceImpl implements BudgetSummaryService {
         this.repo = repo;
     }
 
+    @Override
     public List<?> getAll() {
         return repo.findAll();
+    }
+
+    // ✅ ADD
+    @Override
+    public BudgetSummary create(BudgetSummary summary) {
+        summary.setGeneratedAt(LocalDateTime.now());
+        return repo.save(summary);
     }
 }
