dfb8120751b08b301aba8f748e3485832c342392
bala-amypo <bala@amypo.in>
2025-12-26 07:44:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -0,0 +1,49 @@
+package com.example.demo.model;
+
+import com.example.demo.exception.BadRequestException;
+
+import java.time.LocalDate;
+
+public class TransactionLog {
+
+    private Long id;
+    private User user;
+    private Category category;
+    private Double amount;
+    private String note;
+    private LocalDate transactionDate;
+
+    public TransactionLog() {}
+
+    public TransactionLog(Long id, User user, Category category,
+                          Double amount, String note, LocalDate transactionDate) {
+        this.id = id;
+        this.user = user;
+        this.category = category;
+        this.amount = amount;
+        this.note = note;
+        this.transactionDate = transactionDate;
+    }
+
+    public void validate() {
+        if (transactionDate.isAfter(LocalDate.now())) {
+            throw new BadRequestException("Future date not allowed");
+        }
+        if (amount <= 0) {
+            throw new BadRequestException("Invalid amount");
+        }
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public User getUser() { return user; }
+    public Category getCategory() { return category; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public void setTransactionDate(LocalDate date) {
+        this.transactionDate = date;
+    }
+}
