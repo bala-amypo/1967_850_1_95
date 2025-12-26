dc94eda9d557d6d424b82b924553027037f6633d
bala-amypo <bala@amypo.in>
2025-12-26 07:46:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repo;
+    private final PasswordEncoder encoder;
+
+    public UserServiceImpl(UserRepository repo, PasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    public User register(User user) {
+        if (repo.existsByEmail(user.getEmail())) {
+            throw new BadRequestException("Email exists");
+        }
+        user.setId(null);
+        return repo.save(user);
+    }
+}
