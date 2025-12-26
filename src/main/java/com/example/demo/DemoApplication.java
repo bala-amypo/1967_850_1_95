5aadc74acb80c21e66924e2688e693c7e3c259cb
bala-amypo <bala@amypo.in>
2025-12-26 03:49:32 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,15 @@
+
 package com.example.demo;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class DemoApplication implements ServletContextInitializer {
+@ServletComponentScan
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
     }
 }
