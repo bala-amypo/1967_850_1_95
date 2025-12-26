80dc66160831f2f416ceb445421c0bea6a3d386a
bala-amypo <bala@amypo.in>
2025-12-26 09:14:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TransactionServiceImpl.java
@@ -1,7 +1,18 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
+import com.example.demo.model.Transaction;
+import com.example.demo.service.TransactionService;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
+
+    @Override
+    public List<Transaction> getUserTransactions(Long userId) {
+        // minimal stub
+        return Collections.emptyList();
+    }
 }
