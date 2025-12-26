21cbadfaab72be09ec821be1b81cafab19a84ba9
bala-amypo <bala@amypo.in>
2025-12-26 08:33:21 +0000

    User commit

--- src/main/java/com/example/demo/service/CategoryService.java
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -1,28 +1,9 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
-import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.Category;
-import com.example.demo.repository.CategoryRepository;
-import com.example.demo.service.CategoryService;
-
 import java.util.List;
 
-public class CategoryServiceImpl implements CategoryService {
-    private final CategoryRepository repo;
-
-    public CategoryServiceImpl(CategoryRepository r) {
-        this.repo = r;
-    }
-
-    public Category addCategory(Category c) {
-        if (repo.existsByName(c.getName())) {
-            throw new BadRequestException("Duplicate category");
-        }
-        c.validateType();
-        return repo.save(c);
-    }
-
-    public List<Category> getAllCategories() {
-        return repo.findAll();
-    }
+public interface CategoryService {
+    Category addCategory(Category category);
+    List<Category> getAllCategories();
 }
