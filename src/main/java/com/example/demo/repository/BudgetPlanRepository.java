76236a576c1d8618315ca50cc644466229097292
bala-amypo <bala@amypo.in>
2025-12-26 08:14:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/BudgetPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.*;
-import java.util.Optional;
-
-public interface BudgetPlanRepository {
-    Optional<BudgetPlan> findByUserAndMonthAndYear(User user, int month, int year);
-    BudgetPlan save(BudgetPlan plan);
-}
