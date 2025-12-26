5cfeac8e32d7a347e47c5d4ed19339df943b7ba4
bala-amypo <bala@amypo.in>
2025-12-26 07:46:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.repository.*;
+import com.example.demo.service.BudgetSummaryService;
+
+public class BudgetSummaryServiceImpl implements BudgetSummaryService {
+
+    public BudgetSummaryServiceImpl(BudgetSummaryRepository a,
+                                    BudgetPlanRepository b,
+                                    TransactionLogRepository c) {
+    }
+}
