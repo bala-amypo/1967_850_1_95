e31708027ac5e18093d50ed24a5a553f52bf9b8a
bala-amypo <bala@amypo.in>
2025-12-26 08:33:33 +0000

    User commit

--- src/main/java/com/example/demo/service/TransactionService.java
+++ src/main/java/com/example/demo/service/TransactionService.java
@@ -1,31 +1,9 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.model.TransactionLog;
-import com.example.demo.model.User;
-import com.example.demo.repository.TransactionLogRepository;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.TransactionService;
-
 import java.util.List;
 
-public class TransactionServiceImpl implements TransactionService {
-
-    private final TransactionLogRepository repo;
-    private final UserRepository userRepo;
-
-    public TransactionServiceImpl(TransactionLogRepository r, UserRepository u) {
-        this.repo = r;
-        this.userRepo = u;
-    }
-
-    public TransactionLog addTransaction(Long userId, TransactionLog t) {
-        User u = userRepo.findById(userId).orElseThrow();
-        t.validate();
-        return repo.save(t);
-    }
-
-    public List<TransactionLog> getUserTransactions(Long userId) {
-        User u = userRepo.findById(userId).orElseThrow();
-        return repo.findByUser(u);
-    }
+public interface TransactionService {
+    TransactionLog addTransaction(Long userId, TransactionLog log);
+    List<TransactionLog> getUserTransactions(Long userId);
 }
