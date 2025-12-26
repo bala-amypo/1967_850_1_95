bf082d635fb092cad451aefccc0b6c4db9f55877
bala-amypo <bala@amypo.in>
2025-12-26 03:17:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/BudgetPlanRepository.java
+++
@@ -1,8 +0,0 @@
-// 🟢 NEW FILE (REQUIRED)
-package com.example.demo.repository;
-
-import com.example.demo.model.BudgetPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetPlanRepository extends JpaRepository<BudgetPlan, Long> {
-}
