d6781d7f9959eaa0b7d83d965c94271bc8f0f1af
bala-amypo <bala@amypo.in>
2025-12-26 09:14:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,18 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Override
+    public List<Category> getAllCategories() {
+        // minimal stub
+        return Collections.emptyList();
+    }
 }
