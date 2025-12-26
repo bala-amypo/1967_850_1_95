3685f130f080b660a552818a6a5a8c5119ab8091
bala-amypo <bala@amypo.in>
2025-12-26 06:13:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.User;
+import java.util.Optional;
+
+public interface UserRepository {
+    boolean existsByEmail(String email);
+    Optional<User> findById(Long id);
+    User save(User user);
+}
