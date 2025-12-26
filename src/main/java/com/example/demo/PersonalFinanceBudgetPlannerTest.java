49ef2de702fc79b67ff30a1766f7adbd4f88e31d
bala-amypo <bala@amypo.in>
2025-12-26 08:07:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/PersonalFinanceBudgetPlannerTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PersonalFinanceBudgetPlannerTest {
+
+    @Test
+    public void sampleTest() {
+        Assert.assertTrue(true);
+    }
+}
