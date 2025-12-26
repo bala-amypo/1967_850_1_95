72a525d6a79307f159a84bb653addc4a194f7f91
bala-amypo <bala@amypo.in>
2025-12-26 05:51:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/BudgetSummaryService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.BudgetSummary;
+
+public interface BudgetSummaryService {
+
+    BudgetSummary generateSummary(Long budgetPlanId);
+
+    BudgetSummary getSummary(Long budgetPlanId);
+}
