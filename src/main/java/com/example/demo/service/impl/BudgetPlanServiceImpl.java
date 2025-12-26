d0c760391f4db2008f6f6a68ab3063b8b826e156
bala-amypo <bala@amypo.in>
2025-12-26 09:14:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetPlanServiceImpl.java
@@ -1,7 +1,15 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
+import com.example.demo.model.BudgetPlan;
+import com.example.demo.service.BudgetPlanService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BudgetPlanServiceImpl implements BudgetPlanService {
+
+    @Override
+    public BudgetPlan createBudgetPlan(Long userId, BudgetPlan budgetPlan) {
+        // minimal stub to satisfy compiler
+        return budgetPlan;
+    }
 }
