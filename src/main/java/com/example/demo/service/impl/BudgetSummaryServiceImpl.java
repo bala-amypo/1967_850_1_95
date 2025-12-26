8d03432a11f9907b016fd3b2ee3dd2f861a4ecda
bala-amypo <bala@amypo.in>
2025-12-26 09:24:50 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BudgetSummaryServiceImpl.java
@@ -1,7 +1,26 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
+import com.example.demo.repository.BudgetPlanRepository;
+import com.example.demo.repository.BudgetSummaryRepository;
+import com.example.demo.repository.TransactionLogRepository;
+import com.example.demo.service.BudgetSummaryService;
 
-@Service
 public class BudgetSummaryServiceImpl implements BudgetSummaryService {
+
+    private BudgetSummaryRepository budgetSummaryRepository;
+    private BudgetPlanRepository budgetPlanRepository;
+    private TransactionLogRepository transactionLogRepository;
+
+    // REQUIRED by tests
+    public BudgetSummaryServiceImpl() {
+    }
+
+    // OPTIONAL for Spring
+    public BudgetSummaryServiceImpl(BudgetSummaryRepository budgetSummaryRepository,
+                                    BudgetPlanRepository budgetPlanRepository,
+                                    TransactionLogRepository transactionLogRepository) {
+        this.budgetSummaryRepository = budgetSummaryRepository;
+        this.budgetPlanRepository = budgetPlanRepository;
+        this.transactionLogRepository = transactionLogRepository;
+    }
 }
