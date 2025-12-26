3ef10b8d3ca5bfa534845f6b34806adb6a26ebf8
bala-amypo <bala@amypo.in>
2025-12-26 07:45:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/BudgetSummaryRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.BudgetPlan;
+import com.example.demo.model.BudgetSummary;
+import java.util.Optional;
+
+public interface BudgetSummaryRepository {
+    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan plan);
+}
