4cbdffa223c4ba6821e450605f0ab95f7e9b7709
bala-amypo <bala@amypo.in>
2025-12-27 05:01:53 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -10,7 +10,7 @@ public class TransactionLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+@com.fasterxml.jackson.annotation.JsonIgnore
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
